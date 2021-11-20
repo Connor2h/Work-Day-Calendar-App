33086f58b8293e3e0748781e4151d9fb72d77b51
Connor Hollis <connorhollis3d@gmail.com>
2021-11-20 10:06:01 -0700

    refatored code with TAs help

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,57 @@
 var date = new Date();
-var hoursArray= [9, 10, 11, 12, 1, 2, 3, 4, 5]
+var hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+var hourHolderEl = document.getElementById("hour-holder");
 
 var tasks = {};
 
+// var hourInADay = 9;
 
-document.getElementById('currentDay').innerHTML=date;
+for (var i = 0; i < hoursArray.length; i++) {
+    //create elements
+    var newHourDivEl = document.createElement("div");
+    var newTextAreaEl = document.createElement("textarea");
+    var newButtonEl = document.createElement("button");
+    
+
+    //append elements
+    hourHolderEl.append(newHourDivEl);
+    hourHolderEl.append(newTextAreaEl);
+    hourHolderEl.append(newButtonEl);
+
+    // document.getElementById("demo").id = "newid";
+    //assign id
+    var newHourId = hoursArray[i].toString();
+    newHourDivEl.id = newHourId;
+    newTextAreaEl.id ="textarea-" + i;
+
+    //assign classes
+    newHourDivEl.className = "col-2 hour";
+    newTextAreaEl.className = "col-8";
+    newButtonEl.className = "col-2 saveBtn";
+    
+    console.log(newHourId);
+
+    //check
+    var hourEl = document.getElementById(newHourId);
+    if (hoursArray[i] < 12) {
+
+        hourEl.innerText = newHourId + " AM";
+    } else {
+        hourEl.innerText = newHourId + " PM";
+    }
+
+
+}
+
+
+document.getElementById('currentDay').innerHTML = date;
 
 
 
 
-$(".row").on("click", "div", function() {
+$(".row").on("click", "div", function () {
     var text = $(this).text().trim();
+    // console.log(text)
 
     var textInput = $("<textarea>").addClass("form-control").val(text);
 
@@ -20,7 +61,7 @@ $(".row").on("click", "div", function() {
 
 })
 
-$(".row").on("blur", "textarea", function() {
+$(".row").on("blur", "textarea", function () {
     // get the textarea's current value/text
     var text = $(this).val().trim();
 
@@ -39,7 +80,7 @@ $(".row").on("blur", "textarea", function() {
 
     //important for assignment 5 // may need to wrap this in an event function that if they click the save button then save to local storage/else do nothing
     // recreate p element
-    var taskP = $("<div>").addClass("col-8 past").text(text);
+    var taskP = $("<textarea>").addClass("col-8 future").text(text);
     //var taskP = $("<p>").addClass("m-1").text(text);
 
     // replace textarea with p element
@@ -49,8 +90,8 @@ $(".row").on("blur", "textarea", function() {
 
 
 //I need a click event on the button to save to local storage
-$( ".saveBtn" ).click(function() {
-    alert( "Handler for .click() called." );
+$(".saveBtn").click(function () {
+    alert("Handler for .click() called.");
 });
 
 
